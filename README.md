1dfc6e56a0f851b55e4ee58b6d84cacfc4e8f1c0
Zero <24566018+ZeroLP@users.noreply.github.com>
2022-11-08 19:34:12 +1100

    Local import

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# SharpNativeDLL
- NativeAOT assisted native .NET DLL injection
+#  SharpNativeDLL
+NativeAOT assisted native .NET DLL injection
+
+This is an example project to inject a NAOT compiled native .NET DLL.
+It implements a custom UnmanagedCallersOnly DllMain entrypoint which handles DLL_PROCESS_ATTACH protocol, and can be modified to further accept other protocols. 
+
+# Screenshot
+![](https://github.com/ZeroLP/SharpNativeDLL/blob/main/Example.png)
---
+++ SharpNativeDLL/SharpNativeDLL/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SharpNativeDLL
+{
+    public class EntryPoint
+    {
+        private const uint DLL_PROCESS_DETACH = 0,
+                           DLL_PROCESS_ATTACH = 1,
+                           DLL_THREAD_ATTACH = 2,
+                           DLL_THREAD_DETACH = 3;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        [UnmanagedCallersOnly(EntryPoint = "DllMain", CallConvs = new[] { typeof(CallConvStdcall) })]
+        public static bool DllMain(IntPtr hModule, uint ul_reason_for_call, IntPtr lpReserved)
+        {
+            switch (ul_reason_for_call)
+            {
+                case DLL_PROCESS_ATTACH:
+                    MessageBox(0, "DLL_PROCESS_ATTACH", "Your kind @", 0);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+}
